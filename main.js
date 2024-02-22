2c1823fcfc0427179768ead06e31c374e9b0f96f
Al-Homede-1 <155636135+Al-Homede-1@users.noreply.github.com>
2024-02-22 22:19:06 +0300

    Git my js

---
+++ main.js
@@ -0,0 +1,740 @@
+// // Wait The Window To Load
+// window.onload = function () {
+// // Single Line Comment
+// document.querySelector("h1").style.color = "red"; };
+// window.alert("20+20=30");
+
+
+// console.table(["dfuid", "fsdkgbh", "dksfj"])
+
+
+// console.log("%cElzero %cWeb %cSchool", "font-size: 40px; color: red;", "font-size: 40px; color: green; font-weight: bold;",
+// "font-size: 40px; color: white; background-color: blue;")
+
+
+
+// console.group();
+// console.log("bac #2");
+// console.groupEnd();
+// console.group();
+// console.log("vsdm")
+// console.groupEnd();
+
+// console.group("Group 1");
+// console.log("Message 1");
+// console.log("Message 2");
+// console.group("Child Group");
+// console.log("Message 1");
+// console.log("Message 2");
+// console.group("Geand Child Group");
+// console.log("Message 1");
+// console.log("Message 2");
+// console.groupEnd();
+// console.groupEnd();
+// console.groupEnd();
+// console.group("Group 2");
+// console.log("Message 1");
+// console.log("Message 2");
+
+// var myname = "mohammed";
+// console.log ("Hello " + myname);
+
+
+
+// var userName = 13;
+// var userName = 12;
+// var userName = 10;
+// console.log(userName);
+
+
+
+// let man = 14;
+// console.log(man);
+
+
+
+// const nane = 1;
+// console.log(nane);
+
+
+// console.log("man'man'");
+
+// console.log('man "jaj"');
+
+// console.log("man\"man\"");
+
+// console.log('jhs \'kjaj\'');
+
+// console.log("\\makn")
+
+
+
+// console.log( typeof ("Mohammed"));
+// console.log( typeof 569);
+// console.log( typeof [ "snm" , "skjh" , "kj" ]);
+// console.log( typeof { name: "Mohammed" , age: 19 , cantry: "Yemen"});
+// console.log( typeof true );
+// console.log( typeof false );
+
+
+// var man = "mohammed" , ans = 15; 
+
+// console.log (man);
+// console.log (ans);
+// console.log (man);
+// console.log (ans);
+
+// const aabb = 12
+// console.log(aabb);
+
+// console.log("ksjd\n jdksjbjdf\nsjkdfb")
+
+
+
+// var a = "We love";
+// var b = "javascript"
+// var c = "And";
+// var d = "programming";
+
+
+// console.log (a + " " + b + " \"\" " + c + " " + d );
+// console.log (`${a} ${b} \\ "" ${c} ${d}`)
+
+
+// let pan = "Mohammed";
+// let pans = " kjsu uus nsjk";
+
+// let part = `
+
+// <div class" peer ">
+//     <div class" child ">
+//         <h2>${pan}</h2>
+//         <p>${pans}</p>
+//     </div>
+// </div>
+// `;
+
+// document.write(part);
+
+
+// let the_p = "Hello Elzero", the_l = "Elzero Web School", the_s = "25/10";
+
+// let part =
+// `
+// <div class" card ">
+//         <h2>${the_p}</h2>
+//         <p>${the_l}</p>
+//         <span>${the_s}</span>
+// </div>
+// <div class" card ">
+//         <h2>${the_p}</h2>
+//         <p>${the_l}</p>
+//         <span>${the_s}</span>
+// </div>
+// <div class" card ">
+//         <h2>${the_p}</h2>
+//         <p>${the_l}</p>
+//         <span>${the_s}</span>
+// </div>
+// <div class" card ">
+//         <h2>${the_p}</h2>
+//         <p>${the_l}</p>
+//         <span>${the_s}</span>
+// </div>
+// `
+
+// document.write (part);
+
+
+// let Number_One = 10 ;
+// let Number_Two = 20 ;
+
+// console.log (Number_One+""+Number_Two);
+// console.log ( typeof(Number_One +""+ Number_Two));
+// console.log (`${Number_One}${Number_Two}`)
+// console.log ( typeof(`${Number_One}${Number_Two}`))
+
+
+// console.log ( Number_One + "\n" + Number_Two)
+
+
+// console.log ( `${Number_One}
+// ${Number_Two}`)
+
+// console.log( elzero.innerHTML ); // object
+// console.log(typeof elzero); // object
+
+
+// console.log (`\I'm In
+// \\\\
+// Love \\\\ \"\"\" \'\'\'
+// ++ With ++
+// \\\"\"\"\\\"\"\"
+// \"\"JavaScript\"\"\``);
+
+
+
+// let a = 21;
+// let b = 20;
+
+// console.log(`_${a}_${a}${b}_${a}${b}_${a}${b}_${b}_`);
+
+
+
+// _21_2021_2021_2021_20_
+
+
+// Replace ? With Arithmetic Operators
+// console.log(10 + 20 + 15 - 3 + 190 - 10  400); // 0
+
+
+// console.log (10 + 20 );
+// console.log ( 10 + "mohammed");
+
+// console.log (10 - 20 );
+// console.log ( 10 - "mohammed");
+
+// console.log (typeof NaN);
+
+
+// console.log (10 * 20 );
+// console.log ( 10 * "mohammed")
+
+// console.log ( 20 / 10 );
+// console.log ( 20 / "mohammed" );
+
+// console.log ( 20 % 3 );
+// console.log (10 % 20 );
+
+
+// console.log(2 ** 2);
+// console.log(2 * 2 * 2 * 2);
+
+
+// var man = 1 ;
+// console.log (--man);
+// console.log (man--);
+// console.log (++man);
+// console.log (man++);
+// console.log (man)
+
+
+
+// console.log(+100);
+// console.log(+"100");
+// console.log(+"-100");
+// console.log(+"Mohammed");
+// console.log(+"14.5");
+// console.log(-"-14");
+// console.log(-"moahmede");
+
+// console.log (-null)
+// console.log (-true)
+// console.log (-false)
+
+
+// var a = "10" , b = 20 , c = true ;
+
+
+// console.log (a - -b);
+
+
+
+// let a = 10 ;
+
+// a = a + 20;
+
+// a = a + 70;
+
+// a += 100;   
+
+// // a = a - 50;
+// // a -= 50;
+
+// // a = a / 50 ;
+// // a /= 50;
+
+
+// console.log(a);
+
+
+
+
+// let d = "-100" , e ="20" , f = 30 , g = true ;
+
+
+// console.log(-d * +e) // 2000 
+// console.log( ++e * ++g + -d + ++f) // 173 
+
+// Replace ? With Arithmetic Operators
+// console.log(10 % 20 % 15 % 2 % 190 % 10 % 400) ; // 0
+
+
+// let num = 3;
+
+// // Solution One
+// console.log ( num + num ); // 6
+
+// // Solution Two
+// console.log( num * num - num ); // 6
+
+// // Soultion Three
+// console.log( num * --num); // 6
+
+// // Soultion Four
+// console.log(); // 6
+
+// // Solution Five
+// console.log(); // 6
+
+// // Solution Six
+// console.log(); // 6
+
+
+
+// console.log (1000000);
+// console.log (1_000_000);
+// console.log (1e6);
+// console.log (10 ** 6)
+// console.log (10 * 10 * 10 * 10 * 10 * 10 )
+
+// console.log (1000000.)
+
+// console.log(Number.MAX_SAFE_INTEGER + 57893499057345)
+
+// console.log (Number.MAX_VALUE + 37934799568)
+
+
+// console.log ((10000).toString());
+// console.log (10000..toString());
+
+// console.log (1000.657.toFixed(2));
+
+// console.log (parseInt("100.988 mohmmed "));
+// console.log (parseFloat("100.56 mohmmed "))
+
+// console.log (Number.isNaN("100"));
+// console.log (Number.isNaN(100.566));
+// console.log (Number.isNaN(+"jksdf"));
+
+// console.log (Math.round(99.2))
+// console.log (Math.round(99.5))
+
+// console.log (Math.ceil(99.2))
+// console.log (Math.ceil(99.5))
+
+// console.log (Math.floor(99.2))
+// console.log (Math.floor(99.9))
+
+// console.log (Math.max(10 , 378 ,78 ,578 , 9 ,));
+// console.log (Math.min(10 , 378 ,78 ,578 , 9 ,));
+
+// console.log (Math.pow(2 ,4));
+// console.log (Math.pow(2 ,8));
+
+
+// console.log (Math.random());
+
+// console.log (Math.trunc(75.578))
+
+
+// let man = "MOhammed";
+
+// console.log(man[7])
+// console.log(man)
+
+// console.log (man.charAt(7))
+
+
+// console.log (man.length)
+
+
+
+// document.write("<h1>Elzero</h1>");
+// document.querySelector("h1").style.color = "red";
+// document.querySelector("h1").style.fontSize = "51px";
+// document.querySelector("h1").style.fontWeight = "bold";
+
+// window.alert("Hi Theer How are you ");
+
+// document.write("")
+
+// console.log ("")
+
+// console.log("log");
+// console.error("Error");
+// console.table(["Man" , "Women" , "Ahmed"]);
+// console.log ("Man At %cThe Women" ,  "color: red; font-size: 40px");
+
+// document.write("<h1>Elzero</h1>");
+// document.querySelector("h1").style.color = "red";
+// document.querySelector("h1").style.fontSize = "80px";
+// document.querySelector("h1").style.fontWeight = "blod";
+// document.querySelector("h1").style.textAlign = "center";
+// document.querySelector("h1").style.fontFamily = "";
+
+// console.log(typeof "mMOhamed" );
+// console.log(typeof 13);
+// console.log(typeof 13.64);
+// console.log(typeof [35]);
+// console.log(typeof ["hsd"]);
+// console.log(typeof false);
+// console.log(typeof undefined);
+// console.log(typeof null);
+
+// var man = "MOhmmed ";
+// console.log (man + man + man + man + man);
+// console.log(Nan);
+// Nan.innerHTML = " No "
+// var man = "Mohammed ";
+// var man = "manl  ";
+// console.log( man + man );
+// console.log("pnajkk\"\"");
+// console.log(`djkfjh " ' '/ ] ] `)
+// let man = "I am the";
+// let mna = "Number One";
+
+
+
+// console.log(man +" "+ mna);
+// console.log(`${man} ${mna}`);
+
+
+
+// let a = "10";
+// let b = 20;
+// let c = true;
+
+// console.log (Number(a) + b);
+
+// b /= 20 ;
+
+
+// console.log(b);
+
+// console.log((100).toString())
+// console.log(100..toString())
+// console.log(100.6326.toFixed(3))
+
+
+
+// // Examples
+// console.log(1_000_000); // 100000
+// console.log(1000000); // 100000
+// console.log(5e5 + 5e5 ); // 100000
+
+// // Your Solutions
+// console.log(+"1000000"); // 100000
+// console.log(Number(1000000)); // 100000
+// console.log(); // 100000
+// console.log(); // 100000
+// console.log(); // 100000
+// console.log(); // 100000
+// console.log(); // 100000
+// console.log(); // 100000
+// console.log(); // 100000
+// console.log(); // 100000
+
+
+// let man = "  Ahmed  ";
+// console.log(man[1])
+// console.log(man.charAt(1))
+// console.log(man.length)
+
+
+// console.log(a.indexOf("o"));
+
+// console.log(a.lastIndexOf("o"));
+
+// console.log(a.slice(-6));
+
+
+// console.log(a.repeat(3));
+// console.log(a.split(" ",2))
+
+// let a = "Elzero Web school";
+// console.log(a.length)
+
+
+// console.log(a.substring(0,6));
+// console.log(a.substring(a.length -1));
+
+
+// console.log(a.substr(0,6));
+// console.log(a.includes("Web"));
+// console.log(a.startsWith("Web"));
+// console.log(a.charAt(2).repeat(8));
+
+
+
+// console.log(10 != "10");
+// console.log("amnd" != 10);
+// console.log("10" != 12);
+
+
+// console.log(100 == "100"); // true
+// console.log(100 < 1000); // true
+// console.log(110 > 100 < 10 < 20); // true
+// console.log(-10 == "-10"); // true
+// console.log( -50  <= +"-40"); // true
+// console.log( 10 < -"-40"); // true
+// console.log( "10" == 10); // true
+// console.log( 20 !== false); // true
+
+// let price = 100 ;
+// let discount = false ;
+// let discountAmount = 30;
+// let country = "man";
+
+// if (discount === true ) {
+//     price -= discountAmount ;
+// }else if (country === "Yemen") {
+//     price -= 50;
+// }else if (country === "ksa"){
+//     price +=20 ;
+// }else {
+//     price -= 10;
+// }
+// console.log(price)
+
+// let a = "Sara";
+// let b = "FeMale"
+// let c = 60;
+
+// if (b === "Male"){
+//     console.log("Mr")
+// }else{
+//     console.log("Mrs");
+// }
+// b === "Male" ? console.log("Mr") : console.log("Mrs");
+// let pan = b === "Male" ? "Mr" :"Mrs";
+
+// console.log(`Hello ${pan} ${a}`);
+
+// c <= 20 ? console.log(20) : c > 20 && c < 60 ? console.log("20 To 60") : c >= 60 ? console.log("Larger than 60") : console.log("Die for you");
+
+// let price;
+
+// console.log(`The price is ${price || 120}`);
+// console.log(`The price is ${price ?? 150}`);
+
+// let day = 1;
+
+
+// switch (day){
+//     case 1:
+//         console.log("Satrdy");
+//         break;
+//     case 1:
+//         console.log("sundye");
+//         break;
+//     case 2:
+//     case 3:
+//         console.log("mondye");
+//         break;
+//     default: 
+//     console.log("ande finde")
+//     break;
+// }
+
+// let myfrinds = ["Ahmed" , "Mohammed" , "Manal" , "Manal" , "Ahmed"];
+// console.log(myfrinds);
+
+// console.log(myfrinds);
+// myfrinds[myfrinds.length]= "mazen";
+// // myfrinds[myfrinds.length]= "mana";
+// console.log(myfrinds);
+// myfrinds.pop();
+
+// console.log(myfrinds);
+
+// console.log(myfrinds.lastIndexOf("Ahmed", -2));
+// console.log(myfrinds.indexOf("Ahmed", 2));
+// console.log(myfrinds.includes("Ahmed"));
+
+
+// let myfrinds = [10 , "Mohammed" , "Ahmed" , "90", -22 , -12 , 15 , 636 , "Mnal"];
+// console.log(myfrinds);
+
+
+// console.log(myfrinds.sort());
+// console.log(myfrinds.reverse());
+
+// let myfrinds = ["man" , "khb" , "jdkfbn", "jdkfn" ];
+// let color = ["balc", "red" , "blue"];
+// let size  = [12 , 15 ,17];
+// for ( i = 0; i < myfrinds.length; i++ ){
+//     console.log("");
+//     console.log(myfrinds[i]);
+//     for ( a = 0; a < color.length; a++){
+//         console.log(color[a])
+//     }
+//     for ( b = 0; b < size.length; b++){
+//         console.log(size[b])
+//     }
+// }
+// let i = 0;
+
+// while (i <11 ) {
+//     console.log(i);
+//     i++;
+// }
+// do {
+//     console.log(i);
+//     i++;
+// }while(false);
+// console.log(i);
+
+
+
+// function sayhrllo(userName){
+//     console.log(`Hello ${userName}`);
+// }
+
+// sayhrllo("Ahmed");
+// sayhrllo("A");
+// sayhrllo("Ahmed");
+// sayhrllo("Ahmed");
+// sayhrllo("Ahmed");
+// sayhrllo("Ahmed");
+
+// let user = {
+//     theName: "Mohammed",
+//     theAge: 19,
+//     sayHello: function () {
+//         return `Hello`;
+//     },
+// }; 
+
+// console.log(user.theName);
+// console.log(user.theAge);
+// console.log(user.sayHello());
+
+// let available = true;
+
+
+
+// let user = {
+//     name: "Mohammed",
+//     age: 19,
+//     skills: ["Html", "Css", "js"],
+//     available: false,
+//     addresses: {
+//         kas: "Riyadh",
+//         Yemen: {
+//             one: "Taiz",
+//             Tow: "Sana",
+//         },
+//     },
+//     checkAv: function () {
+//         if (available === true) {
+//             return `Fre For Work`;
+//         }else {
+//             return `Not Fre`;
+//         }
+//     }
+// };
+
+
+// console.log(user.name);
+// console.log(user.age);
+// console.log(user.skills);
+// console.log(user.skills.join(" | "));
+// console.log(user.skills[2]); // Access With Index
+// console.log(user.addresses.kas);
+// console.log(user.addresses.Yemen.one);
+// console.log(user["addresses"].Yemen.one);
+// console.log(user["addresses"]["Yemen"]);
+// console.log(user["addresses"]["Yemen"]["one"]);
+
+// console.log(user.checkAv());
+
+
+// let user = new Object({
+
+// });
+// console.log(user);
+
+// user.age = 19 ;
+// user.name = "mohammed";
+
+
+let man = document.querySelector("p");
+man.innerHTML = "Man is am <span>sex porn<span/> pan"
+console.log(man);
+
+
+man.textContent = "Man is am <span>sex porn<span/> pan"
+console.log(man);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
