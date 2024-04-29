40da4103b3769891d1246c62eb4738fe80e3458d
Al-Homede-1 <155636135+Al-Homede-1@users.noreply.github.com>
2024-04-29 10:56:24 +0300

    I learn about validit form and  preventDefault

--- main.js
+++ main.js
@@ -1491,26 +1491,36 @@
 //       shaow.style.objectFit = "cover";
 //   }
 // };
-let userinput = document.querySelector('[name="username"]');
-let ageinput = document.querySelector('[name="age"]');
 
-document.forms[0].onsubmit = function (e) {
-    let userValid = false;
-    let ageValid = false;
-    if (userinput.value !== "" && userinput.value.length <= 10) {
-      userValid = true;
-    };
-    if (userinput.value !== "") {
-      ageValid = true;
-    };
-    if (userValid === false || ageValid === false) {
-      e.preventDefault();
-    };
-}
-
-document.links[0].onclick = function (event) {
-  event.preventDefault();
-  console.log(event);
-}
-
-
+// document.forms[0].onsubmit = function (e) {
+  //     let userValid = false;
+//     let ageValid = false;
+//     if (userinput.value !== "" && userinput.value.length <= 10) {
+  //       userValid = true;
+//     };
+//     if (userinput.value !== "") {
+  //       ageValid = true;
+  //     };
+//     if (userValid === false || ageValid === false) {
+  //       e.preventDefault();
+  //     };
+  // }
+  
+  // document.links[0].onclick = function (event) {
+    //   event.preventDefault();
+    //   console.log(event);
+    // }
+    
+    let userinput = document.querySelector('[name="username"]');
+    let ageinput = document.querySelector('[name="age"]');
+    
+    userinput.addEventListener('clikc', function() {
+      // Get the value of the first input field
+      let value = input1.value;
+      
+      // Check if the value contains only one number
+      if(/^\d$/.test(value)) {
+          // If it does, focus on the second input field
+          ageinput.focus();
+      }
+  });
