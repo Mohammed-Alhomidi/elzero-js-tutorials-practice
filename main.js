cc6ef597a3cf2e05a8287cbc179c153f42c21cfe
Al-Homede-1 <155636135+Al-Homede-1@users.noreply.github.com>
2024-06-07 14:29:34 +0300

    adding fex in popup pages

--- main.js
+++ main.js
@@ -1937,7 +1937,8 @@
 
 // console.log([..."mohammed"]);
 // console.log([...myarry]);
-   
+    
+        
 // let n1 = [10, 30, 10, 20];
 // let n2 = [30, 20, 10];
 // console.log(Math.max(...n1) * [...n1 , ...n2].length); // 210
